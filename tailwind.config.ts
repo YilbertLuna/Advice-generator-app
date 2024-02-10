abbef98981d686e94464bafdec720ebbd3003aae
Yilbert Luna <yilbert.luna.e@gmail.com>
2024-02-10 17:25:37 -0400

    adding styles for the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        'Mobile': '375px',
+        'Desktop': '1440px'
       },
+      colors: {
+        'DarkGrayishBlue': 'hsl(217, 19%, 24%)',
+        'DarkBlue': 'hsl(218, 23%, 16%)',
+        'LightCyan': 'hsl(193, 38%, 86%)',
+        'NeonGreen': 'hsl(150, 100%, 66%)'
+      },
+      fontSize: {
+        'quote': '28px',
+      },
+      fontFamily: {
+        'Manrope': ['Manrope', 'sans-serif']
+      }
     },
   },
   plugins: [],
